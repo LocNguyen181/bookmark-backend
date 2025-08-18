116e82c051623f7a7619346b4b0700047e0bfdd8
locnt1 <locnt1@runsystem.net>
2025-08-18 14:16:51 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  // Fallback cho React Router
+  app.getHttpAdapter().get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
+
   app.enableCors({
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
