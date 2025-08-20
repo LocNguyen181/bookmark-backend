bbd9d589396f42f7e1fae305f05c32ad53e5d854
locnt1 <locnt1@runsystem.net>
2025-08-20 10:33:54 +0700

    fix CORS config

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,9 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: "https://jpburl.onrender.com",
+    methods: "GET,POST,PUT,DELETE",
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
