e1da15da15bbc9779a63105c7c3441e66af6f2bf
locnt1 <locnt1@runsystem.net>
2025-08-20 10:22:31 +0700

    fix connect DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+        if (!uri) {
+          throw new Error('MONGO_URI is undefined. Please set the environment variable.');
+        }
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
     BookmarksModule,
   ],
 })
